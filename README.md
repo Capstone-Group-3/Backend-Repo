d39c14f69d3c299ce961f066c1af3997c17f1ede
nickbloo <nickbloo2@gmail.com>
2022-11-21 15:28:29 -0600

    initial setup

---
+++ README.md
@@ -0,0 +1,12 @@
+## git checkout -b `whatever you want to call your branch`
+
+## First: npm init -y
+
+## In command line, npm install:
+- npm install express
+- morgan
+- jsonwebtoken
+- dotenv
+- pg
+- nodemon --save -dev
+- bcrypt
---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+    console.log("This is the body of the request ", req.body);
+    next();
+});
+
+app.get("/", (req, res, next) => {
+    res.send(`<h1>Welcome to the homepage!</h1>`);
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`)
+});
